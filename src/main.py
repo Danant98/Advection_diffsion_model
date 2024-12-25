8988740980bed6126bf779a027f5fc31ba4de265
Daniel E. Antonsen <danieleantonsen@gmail.com>
2024-12-25 16:08:23 +0100

    added solution in 2D using Fourier-Galerkin in space

--- src/main.py
+++ src/main.py
@@ -10,65 +10,126 @@ import matplotlib.pyplot as plt
 class advection_diffusion:
 
     def __init__(self, 
-                 N: int = 64, 
-                 L: float = 4 * np.pi, 
-                 T: float = 1.0, 
+                 Nx: int = 256, 
+                 Ny: int = 256,
+                 Lx: float = 25, 
+                 Ly: float = 25,
+                 T: float = 2.0, 
                  dt: float = 1e-2,
-                 v: float = 1.0,
-                 D: float = 1.0
+                 vx: float = 1.0,
+                 vy: float = 1.0,
+                 D: float = 1.0,
+                 dims: str = '1D'
                  ) -> None:
-        # Defining spacial domain
-        self.x = np.linspace(0, L, N)
-        # Defining time array
-        self.t = np.arange(0, T, dt)
-        self.dt = dt
-        self.L = L
-
-        # Defining wavenumbers
-        self.k = np.fft.fftfreq(N, d = L / N) * 2 * np.pi
+        # Storing dimensions
+        self.dims = dims
         
-        # Computing linear operator in Fourier space (Advection + Diffusion terms)
-        self.L_hat = -(1j * v * self.k + D * self.k**2)
+        if dims == '1D':
+            # Defining spacial domain
+            self.x = np.linspace(0, Lx, Nx)
+            # Defining time step
+            self.dt = dt
+            self.t = np.arange(0, T, dt)
 
-        # Initialize u(x, t)
-        self.u = np.zeros((self.x.shape[0], self.t.shape[0]))
+            # Domain size
+            self.L = Lx
 
-    def u0(self, l: float = 0.1, amp: float = 1.0) -> np.ndarray:
+            # Defining wavenumbers
+            self.k = np.fft.fftfreq(Nx, d = Lx / Nx) * 2 * np.pi
+            
+            # Computing linear operator in Fourier space (Advection + Diffusion terms)
+            self.L_hat = -(1j * vx * self.k + D * self.k**2)
+
+            # Initialize u(x, t)
+            self.u = np.zeros((self.x.shape[0], len(np.arange(0, T, dt))))
+        elif dims == '2D':
+            # Define spatial domain
+            self.x = np.linspace(0, Lx, Nx)
+            self.y = np.linspace(0, Ly, Ny)
+            # Defining time array
+            self.dt = dt
+            self.t = np.arange(0, T, dt)
+
+            # Defining wavenumbers (kx and ky)
+            kx = np.fft.fftfreq(Nx, d = Lx / Nx) * 2 * np.pi
+            ky = np.fft.fftfreq(Ny, d = Ly / Ny) * 2 * np.pi
+            self.kx, self.ky = np.meshgrid(kx, ky, indexing = 'ij')
+
+            # Defining size of domain
+            self.Lx = Lx
+            self.Ly = Ly
+
+            # Computing 2D linear operator in Fourier space (Advection + Diffusion terms) 
+            self.L_hat = -(1j * vx * self.kx + 1j * vy * self.ky + D * (self.kx**2 + self.ky**2))
+
+            # Initializing u(x, y, t)
+            self.u = np.zeros((Nx, Ny, len(np.arange(0, T, dt))))
+
+
+    def u0(self, l: float = 2.0, amp: float = 1.0) -> np.ndarray:
         """
         Inintal conditions; Gaussian distribution
         """
-        return amp * np.exp(-((self.x - (self.L / 2))**2 ) / (2 * l**2))
+        if self.dims == '1D':
+            return amp * np.exp(-((self.x - (self.L / 2))**2 ) / (2 * l**2))
+        elif self.dims == '2D':
+            X, Y = np.meshgrid(self.x, self.y, indexing = 'ij')
+            return amp * np.exp(-( (X - (self.Lx / 2))**2 + (Y - (self.Ly / 2))**2 ) / (2 * l**2) )
+
 
     def solve(self) -> np.ndarray:
         """
         Solving the advection-diffusion equation using Fourier-Galerkin in space
         and backward difference in time
         """
-        # Initial condition u(x, 0)
-        self.u[:, 0] = self.u0()
-        # Fourier transfrom of initial condition
-        uhat = sfft.fft(self.u[:, 0])
+        if self.dims == '1D':
+            # Initial condition u(x, 0)
+            self.u[:, 0] = self.u0()
+            # Fourier transfrom of initial condition
+            uhat = sfft.fft(self.u[:, 0])
 
-        for i in range(1, self.t.shape[0]):
-            uhat = uhat / (1 - self.dt * self.L_hat)
-            self.u[:, i] = np.real(sfft.ifft(uhat))
-        
+            # Looping over time t
+            for i in range(1, self.u.shape[1]):
+                uhat = uhat / (1 - self.dt * self.L_hat)
+                self.u[:, i] = np.real(sfft.ifft(uhat))
+        elif self.dims == '2D':
+            # Initial condition u(x, y, 0)
+            self.u[:, :, 0] = self.u0()
+            # Fourier transfrom of initial condition
+            uhat = sfft.fft2(self.u[:, :, 0])
+
+            # Looping over time t
+            for i in range(1, self.u.shape[2]):
+                uhat = uhat / (1 - self.dt * self.L_hat)
+                self.u[:, :, i] = np.real(sfft.ifft2(uhat))
+            
         return self.u
 
     def animate(self) -> None:
         """
-        Animate the solution of the 1D advection - diffusion equation using dirichlet boundary
+        Animate the solution of the advection - diffusion equation using periodic boundary conditions in spatial domain
         """
-        plt.figure()
-        for i in range(self.t.shape[0]):
-            plt.clf()
-            plt.plot(self.x, self.u[:, i])
-            plt.xlabel(r'$x$')
-            plt.ylabel(r'$u(x, t)$')
-            plt.pause(1E-7)
+        if self.dims == '1D':
+            plt.figure()
+            for i in range(self.u.shape[1]):
+                plt.clf()
+                plt.plot(self.x, self.u[:, i])
+                plt.xlabel(r'$x$')
+                plt.ylabel(r'$u(x, t)$')
+                plt.pause(1E-7)
+        elif self.dims == '2D':
+            plt.figure()
+            for i in range(self.u.shape[2]):
+                plt.clf()
+                plt.contourf(self.x, self.y, self.u[:, :, i])
+                plt.colorbar(label = r'$n(x, y)$')
+                plt.xlabel(r'$x$')
+                plt.ylabel(r'$y$')
+                plt.title(f'Time: {self.t[i]:.3f}')
+                plt.pause(1E-4)
         plt.show()
 
 if __name__ == '__main__':
-    ad = advection_diffusion()
+    ad = advection_diffusion(dims = '2D')
     ad.solve()
     ad.animate()
